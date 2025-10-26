81e288e177a49e50cc40688b9b531086e9c7ced4
chiayinin <chiayinin@gmail.com>
2025-10-26 12:29:23 +0800

    feat: hono is connect to Drizzle DB.

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,17 @@
 import { Hono } from 'hono';
+import { db } from '@/db/drizzle';
+import { accounts } from '@/db/schema';
 
 const app = new Hono()
-  .get('/', (cxt) => {
-    return cxt.json({ accounts: [] });
+  .get('/', async (cxt) => {
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts);
+
+    return cxt.json({ data });
   });
 
 export default app;
--- db/drizzle.ts
+++ db/drizzle.ts
@@ -3,5 +3,5 @@ import { drizzle } from 'drizzle-orm/neon-http';
 
 import * as schema from './schema';
 
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle({ client: sql });
+export const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
