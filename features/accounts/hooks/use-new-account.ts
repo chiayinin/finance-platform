bf8b6d966ddf99f51616645f5f641ce2eb765e68
chiayinin <chiayinin@gmail.com>
2025-10-29 13:55:12 +0800

    component: add feature accounts sheet.

--- features/accounts/hooks/use-new-account.ts
+++ features/accounts/hooks/use-new-account.ts
@@ -9,5 +9,5 @@ type NewAccountState = {
 export const useNewAccount = create<NewAccountState>((set) => ({
   isOpen: false,
   onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
 }))
