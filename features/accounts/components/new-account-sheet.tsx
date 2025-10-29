a6325d3860e10d43b7ffea478f20fd4f2fb05169
chiayinin <chiayinin@gmail.com>
2025-10-29 15:47:08 +0800

    component: account form

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,4 +1,5 @@
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { AccountForm } from "./account-form";
 import {
   Sheet,
   SheetContent,
@@ -6,13 +7,25 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import z from "zod";
+import { insertAccountSchema } from "@/db/schema";
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
 
+  const onSubmit = (values: FormValues) => {
+    console.log({ sheet: values });
+  }
+
   return(
     <Sheet open={isOpen} onOpenChange={onClose}>
-      <SheetContent className=" space-y-4">
+      <SheetContent className="space-y-4">
         <SheetHeader>
           <SheetTitle>Are you absolutely sure?</SheetTitle>
           <SheetDescription>
@@ -20,6 +33,12 @@ export const NewAccountSheet = () => {
             and remove your data from our servers.
           </SheetDescription>
         </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={false}
+          defaultValues={{
+            name: ''
+          }} />
       </SheetContent>
     </Sheet>
   )
