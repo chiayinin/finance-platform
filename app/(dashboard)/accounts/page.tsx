df0b8044ccafe3964c2d25f46923ed113166044a
chiayinin <chiayinin@gmail.com>
2025-10-30 20:27:18 +0800

    page: loading skeleton.

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { columns, Payment } from "./columns";
+import { columns } from "./columns";
 
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -12,35 +12,30 @@ import {
   CardContent
 } from "@/components/ui/card";
 import { DataTable } from "@/components/ui/data-table";
-
-async function getData(): Promise<Payment[]> {
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-  ]
-};
-
-const data: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-  {
-    id: "728ed53f",
-    amount: 200,
-    status: "pending",
-    email: "aaa@example.com",
-  },
-];
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+
+  if(accountsQuery.isLoading) {
+    return(
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48"/>
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex justify-center items-center">
+              <Loader2 className="size-6 text-indigo-300 animate-spin"/>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  };
 
   return(<>
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -56,11 +51,11 @@ const AccountsPage = () => {
         </CardHeader>
         <CardContent>
           <DataTable
-          columns={columns}
-          data={data}
-          filterKey="email"
-          onDelete={() => {}}
-          disabled={false} />
+            columns={columns}
+            data={accounts}
+            filterKey="name"
+            onDelete={() => {}}
+            disabled={false} />
         </CardContent>
       </Card>
     </div>
