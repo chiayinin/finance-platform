76a47607bbb20993f7dba67384a4f0324bc7174b
chiayinin <chiayinin@gmail.com>
2025-11-01 20:36:59 +0800

    api: creat get account id API.

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -32,7 +32,46 @@ const app = new Hono()
 
       return ctx.json({ data });
     })
-  .post('/',
+  .get(
+    '/:id',
+    zValidator('param', z.object({
+      id: z.string().optional(),
+    })),
+    clerkMiddleware(),
+    async (ctx) => {
+      const auth = getAuth(ctx);
+      const { id } = ctx.req.valid('param');
+
+      if(!id) {
+        return ctx.json({ error: "找不到此 ID" }, 400);
+      }
+
+      if(!auth?.userId) {
+        return ctx.json({ error: "未經授權" }, 401);
+      }
+
+      const [data] = await db
+        .select({
+          id: accounts.id,
+          name: accounts.name,
+        })
+        .from(accounts)
+        .where(
+          and(
+            eq(accounts.userId, auth.userId),
+            eq(accounts.id, id)
+          ),
+        );
+
+      if(!data) {
+        return ctx.json({ error: "找不到 404" }, 404);
+      }
+
+      return ctx.json({ data });
+    },
+  )
+  .post(
+    '/',
     clerkMiddleware(),
     zValidator('json', insertAccountSchema.pick({
       name: true,
@@ -53,37 +92,37 @@ const app = new Hono()
 
       return ctx.json({ data });
     })
-    .post(
-      'bulk-delete',
-      clerkMiddleware(), // 驗證使用者的身份，確保使用者已經登入了。
-      zValidator(
-        'json',
-        z.object({
-          ids: z.array(z.string()),
-        }),
-      ),
-      async (ctx) => {
-        const auth = getAuth(ctx);
-        const values = ctx.req.valid('json');
+  .post(
+    'bulk-delete',
+    clerkMiddleware(), // 驗證使用者的身份，確保使用者已經登入了。
+    zValidator(
+      'json',
+      z.object({
+        ids: z.array(z.string()),
+      }),
+    ),
+    async (ctx) => {
+      const auth = getAuth(ctx);
+      const values = ctx.req.valid('json');
 
-        if(!auth?.userId) {
-          return ctx.json({ error: '未經授權' }, 401);
-        }
-
-        const data = await db
-          .delete(accounts)
-          .where( // 設定刪除條件
-            and( // 多個條件都要成立
-              eq(accounts.userId, auth.userId), // 「等於」條件
-              inArray(accounts.id, values.ids) // a 的值在 b 陣列中
-            )
-          )
-          .returning({
-            id: accounts.id,
-          });
-
-        return ctx.json({ data });
+      if(!auth?.userId) {
+        return ctx.json({ error: '未經授權' }, 401);
       }
-    );
+
+      const data = await db
+        .delete(accounts)
+        .where( // 設定刪除條件
+          and( // 多個條件都要成立
+            eq(accounts.userId, auth.userId), // 「等於」條件
+            inArray(accounts.id, values.ids) // a 的值在 b 陣列中
+          )
+        )
+        .returning({
+          id: accounts.id,
+        });
+
+      return ctx.json({ data });
+    }
+  );
 
 export default app;
