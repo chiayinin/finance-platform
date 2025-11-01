ffb91bc9095221659c7e1ce66ac0c9d84a1f3643
chiayinin <chiayinin@gmail.com>
2025-11-01 20:51:51 +0800

    feat: use get account

--- features/accounts/api/use-get-account.ts
+++ features/accounts/api/use-get-account.ts
@@ -1,15 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { client } from "@/lib/hono";
 
-export const useGetAccounts = () => {
+export const useGetAccounts = (id?: string) => {
   const query = useQuery({
-    queryKey: ['accounts'],
+    enabled: !!id,
+    queryKey: ['account', { id }],
     queryFn: async () => {
-      const respones = await client.api.accounts.$get();
+      const respones = await client.api.accounts[':id'].$get({
+        param: { id },
+      });
 
       // 因為不是 axios 不會自動處理錯誤
       if(!respones.ok) {
-        throw new Error("無法取得全部帳戶。");
+        throw new Error("無法取得此帳戶。");
       }
 
       const { data } = await respones.json();
