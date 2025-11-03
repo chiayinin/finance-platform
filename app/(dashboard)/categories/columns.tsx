1c2f2ec34e5a3ba8532234519a29d805336bddfa
chiayinin <chiayinin@gmail.com>
2025-11-03 20:55:20 +0800

    page: Category columns done.

--- app/(dashboard)/categories/columns.tsx
+++ app/(dashboard)/categories/columns.tsx
@@ -9,7 +9,7 @@ import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { ColumnDef } from "@tanstack/react-table";
 
-export type ResponseType = InferResponseType<typeof client.api.accounts.$get, 200>['data'][0];
+export type ResponseType = InferResponseType<typeof client.api.categories.$get, 200>['data'][0];
 
 export const columns: ColumnDef<ResponseType>[] = [
   {
