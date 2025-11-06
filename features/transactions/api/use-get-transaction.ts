c8129d8bc5d875ce4b68507345b67453dbf39918
chiayinin <chiayinin@gmail.com>
2025-11-06 19:31:56 +0800

    feat: create Transactions fetch API.

---
+++ features/transactions/api/use-get-transaction.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { client } from "@/lib/hono";
+
+export const useGetTransaction = (id?: string) => {
+  const query = useQuery({
+    enabled: !!id,
+    queryKey: ['transaction', { id }],
+    queryFn: async () => {
+      const respones = await client.api.transactions[':id'].$get({
+        param: { id },
+      });
+
+      // 因為不是 axios 不會自動處理錯誤
+      if(!respones.ok) {
+        throw new Error("無法取得此交易資訊。");
+      }
+
+      const { data } = await respones.json();
+
+      return data;
+    },
+  });
+
+  return query;
+}
---
+++ features/transactions/api/use-get-transactions.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { client } from "@/lib/hono";
+
+export const useGetTransactions = () => {
+  const params = useSearchParams();
+  const from = params.get('from') || '';
+  const to = params.get('to') || '';
+  const accountId = params.get('accountId') || '';
+
+  const query = useQuery({
+    // TODO: Check if params are needed in the key
+    queryKey: ['transactions', from, to, accountId],
+    queryFn: async () => {
+      const respones = await client.api.transactions.$get({
+        query: {
+          from,
+          to,
+          accountId
+        }
+      });
+
+      // 因為不是 axios 不會自動處理錯誤
+      if(!respones.ok) {
+        throw new Error("無法取得全部的交易資訊。");
+      }
+
+      const { data } = await respones.json();
+
+      return data;
+    },
+  });
+
+  return query;
+}
