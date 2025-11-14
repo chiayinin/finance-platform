8f7981ef476894a52392da5cd24bb3c594aa2273
chiayinin <chiayinin@gmail.com>
2025-11-14 17:57:20 +0800

    page: import table.

---
+++ app/(dashboard)/transactions/import-card.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+import { ImportTable } from "./import-table";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const dateFormat = "yyyy-MM-dd HH:mm:ss";
+const outputFormat = "yyyy-MM-dd";
+
+const requiredOptions = [
+  "amount",
+  "date",
+  "payee",
+];
+
+interface SelectedColumnsState {
+  [key: string]: string | null;
+};
+
+type Props = {
+  data: string[][];
+  onCancel: () => void;
+  onSubmit: (data: any) => void;
+};
+
+export const ImportCard = ({
+  data,
+  onCancel,
+  onSubmit,
+}: Props) => {
+  const [selectedColumns, setSelectedColumns] = useState<SelectedColumnsState>({});
+
+
+  const headers = data[0];
+  const body = data.slice(1);
+
+  const onTableHeadSelectChange = (
+    columnIndex: number,
+    value: string | null
+  ) => {
+    setSelectedColumns((prev) => {
+      const newSelectedColumns = {...prev};
+
+      for(const key in newSelectedColumns) {
+        if(newSelectedColumns[key] === value) {
+          newSelectedColumns[key] = null;
+        }
+      }
+
+      if(value === 'skip') {
+        value = null;
+      }
+
+      newSelectedColumns[`column_${columnIndex}`] = value;
+
+      return newSelectedColumns;
+    });
+  };
+
+  return(
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="lg:flex gap-y-2 lg:flex-row lg:justify-between lg:items-center">
+          <CardTitle className="text-xl line-clamp-1">
+            匯入交易紀錄
+          </CardTitle>
+          <div className="flex flex-col lg:flex-row gap-2">
+            <Button onClick={onCancel} size="sm" className="w-full lg:w-auto">
+              取消
+            </Button>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <ImportTable
+            headers={headers}
+            body={body}
+            selectedColumns={selectedColumns}
+            onTableHeadSelectChange={onTableHeadSelectChange}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 
+import { useState } from "react";
 import { Loader2, Plus } from "lucide-react";
 
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+
 import { columns } from "./columns";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
 
 import { Skeleton } from "@/components/ui/skeleton";
 import { DataTable } from "@/components/data-table";
@@ -17,7 +21,31 @@ import {
   CardContent
 } from "@/components/ui/card";
 
+const enum VARIANTS {
+  LIST = "LIST",
+  IMPORT = "IMPORT"
+};
+
+const INITIAL_IMPORT_RESULTS = {
+  data: [],
+  errors: [],
+  meta: {},
+};
+
 const TransactionsPage = () => {
+  const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST); // 切換為: 表單、匯入，兩種畫面。
+  const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
+  const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+    setImportResults(results);
+    setVariant(VARIANTS.IMPORT);
+  };
+
+  const onCancelImport = () => {
+    setImportResults(INITIAL_IMPORT_RESULTS);
+    setVariant(VARIANTS.LIST);
+  };
+
   const newTransaction = useNewTransaction();
   const transactionsQuery = useGetTransactions();
   const deleteTransactions = useBulkDeleteTransactions();
@@ -41,6 +69,19 @@ const TransactionsPage = () => {
     );
   };
 
+  if(variant === VARIANTS.IMPORT) {
+    return(<>
+      <div>
+        這是用來匯入資料的畫面。
+        <ImportCard
+          data={importResults.data}
+          onCancel={onCancelImport}
+          onSubmit={() => {}}
+        />
+      </div>
+    </>);
+  };
+
   return(<>
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
@@ -48,16 +89,19 @@ const TransactionsPage = () => {
           <CardTitle className="text-xl line-clamp-1">
             歷史交易紀錄
           </CardTitle>
-          <Button onClick={newTransaction.onOpen} size="sm">
-            <Plus className="size-4 mr-2" />
-            新增
-          </Button>
+          <div className="flex flex-col lg:flex-row gap-2">
+            <Button onClick={newTransaction.onOpen} size="sm" className="w-full lg:w-auto">
+              <Plus className="size-4 mr-2" />
+              新增
+            </Button>
+            <UploadButton onUpload={onUpload} />
+          </div>
         </CardHeader>
         <CardContent>
           <DataTable
             columns={columns}
             data={transactions}
-            filterKey="收款人"
+            filterKey="payee"
             onDelete={(row) => {
               const ids = row.map((r) => r.original.id);
 
