d5ba28550791171fdb1ad2480b1c89a947420482
chiayinin <chiayinin@gmail.com>
2025-11-25 15:45:32 +0800

    feat: switch chart type

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,5 +1,13 @@
-import { FileSearch } from "lucide-react";
+import { useState } from "react";
+import { AreaChart, BarChart, FileSearch, LineChart } from "lucide-react";
 
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+ } from "@/components/ui/select";
 import {
   Card,
   CardContent,
@@ -19,13 +27,53 @@ type Props = {
 };
 
 export const Chart = ({ data = [] }: Props) => {
+  const [chartType, setChartType] = useState("area");
+
+  const onTypeChange = (type: string) => {
+    // TODO: Add paywall
+
+    setChartType(type);
+  }
   return(
     <Card className="border-none drop-shadow-sm">
       <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
         <CardTitle className="text-xl line-clamp-1">
           Transactions
         </CardTitle>
-        {/* TODO: Add Select */}
+        <Select
+          defaultValue={chartType}
+          onValueChange={onTypeChange}
+        >
+          <SelectTrigger className="lg:w-auto h-9 rounded-md px-3">
+            <SelectValue placeholder="圖表類型" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="area">
+              <div className="flex items-center">
+                <AreaChart className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">
+                  面積圖
+                </p>
+              </div>
+            </SelectItem>
+            <SelectItem value="line">
+              <div className="flex items-center">
+                <LineChart className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">
+                  折線圖
+                </p>
+              </div>
+            </SelectItem>
+            <SelectItem value="bar">
+              <div className="flex items-center">
+                <BarChart className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">
+                  長條圖
+                </p>
+              </div>
+            </SelectItem>
+          </SelectContent>
+        </Select>
       </CardHeader>
       <CardContent>
         {data.length === 0 ? (
@@ -36,9 +84,11 @@ export const Chart = ({ data = [] }: Props) => {
             </p>
           </div>
         ) : (
-          // <AreaVariant data={data} />
-          // <BarVariant data={data} />
-          <LineVariant data={data} />
+          <>
+            {chartType === "area" && <AreaVariant data={data} />}
+            {chartType === "line" && <BarVariant data={data} />}
+            {chartType === "bar" && <LineVariant data={data} />}
+          </>
         )}
       </CardContent>
     </Card>
